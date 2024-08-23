1501b071f16d0f03ca83569526bca33397f172e7
Kishan Suvarna <kishansuvarna09@gmail.com>
2024-08-23 22:38:04 +0400

    added switch case

--- hello_test.go
+++ hello_test.go
@@ -4,14 +4,20 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris","")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
+	t.Run("say 'Hello, World' in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' in French", func(t *testing.T) {
+		got := Hello("Mark", "French")
+		want := "Bonjour, Mark"
 		assertCorrectMessage(t, got, want)
 	})
 }
